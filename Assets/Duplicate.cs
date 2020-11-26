cd6e20a23f7119fac36898415712092ce4535278
jusjustin <justin4@uic.edu>
2020-11-26 01:39:30 -0600

    Final
    Final Push

---
+++ Assets/Duplicate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicate : MonoBehaviour
+{
+    public GameObject rootObj;
+    public Duplicate script;
+    private bool x;
+
+    [System.Obsolete]
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = rootObj.GetComponent<Duplicate>();
+    }
+
+    void Update(){
+        if(script.isActiveAndEnabled){
+                GameObject duplicate = Instantiate(rootObj);
+                duplicate.SetActive(false);
+                Destroy(duplicate.GetComponent<Duplicate>());
+                duplicate.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                duplicate.SetActive(true);
+                duplicate = null;
+             }
+    }
+
+    private void Destroy(Func<Duplicate> getComponent)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/talk_Student_2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talk_Student_2 : MonoBehaviour
+{
+    // retrieve the player GO
+    void Start() {}
+    void Update() { 
+        if ((!audio.isPlaying) && ((player.transform.position-this.transform.position).sqrMagnitude<2)) {
+            // the player is within a radius of 3 units to this game object
+            Debug.Log("Test");
+            audio.PlayOneShot(audio.clip);
+        }
+    }
+    public GameObject player;
+    public AudioSource audio;
+
+}
